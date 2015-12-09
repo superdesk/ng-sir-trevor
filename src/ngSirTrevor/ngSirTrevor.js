cc469b8a6212e332ba940fa94429791aa37f3013
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2015-12-09 02:19:31 +0200

    fix: add all propreties in the service.

--- src/ngSirTrevor/ngSirTrevor.js
+++ src/ngSirTrevor/ngSirTrevor.js
@@ -5,14 +5,7 @@ angular
         this.$get = function() {
             return window.SirTrevor;
         };
-        this.Blocks = window.SirTrevor.Blocks;
-        this.Block = window.SirTrevor.Block;
-        this.Formatters = window.SirTrevor.Formatters;
-        this.Formatter = window.SirTrevor.Formatter;
-        this.Locales = window.SirTrevor.Locales;
-        this.FloatingBlockControls = window.SirTrevor.FloatingBlockControls;
-        this.SimpleBlock = window.SirTrevor.SimpleBlock;
-        this.DEFAULTS = window.SirTrevor.DEFAULTS;
+        angular.extend(this, window.SirTrevor);
     })
     .provider('SirTrevorOptions', function() {
         var options = {
